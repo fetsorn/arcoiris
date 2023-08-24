1f8f45fc2945b61ce469a992a76a119d48c2c019
fetsorn <fetsorn@gmail.com>
2023-08-24 20:37:24 +0400

    test contract deploy

--- test/Arcoiris.t.sol
+++ test/Arcoiris.t.sol
@@ -1,7 +1,8 @@
 pragma solidity 0.8.19;
 
-import {Test} from "forge-std/Test.sol";.
+import {Test} from "forge-std/Test.sol";
 import {Arcoiris} from "contracts/Arcoiris.sol";
+import {Even} from "contracts/redistributions/Even.sol";
 
 contract ArcoirisTestHarness is Arcoiris {
 }
@@ -10,9 +11,19 @@ contract CalibratorTest is Test {
     ArcoirisTestHarness arcoiris;
 
     function setUp() public {
+        arcoiris = new ArcoirisTestHarness();
     }
 
     function test_createGathering() public {
-        arcoiris.createGathering(address(0), address(0), address(msg.sender), false)
+        Even even = Even(address(0));
+
+        uint256 gatheringID = arcoiris.createGathering(
+            address(0),
+            even,
+            address(msg.sender),
+            false
+        );
+
+        assertEq(gatheringID, 0);
     }
 }
